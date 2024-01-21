d054ea168eceb5ff3017679aceae60aef3fbd095
Chandan <hb7102849@gmail.com>
2024-01-21 20:03:03 +0530

    fixed bug

--- frontend/src/components/home/Footer/Footer.js
+++ frontend/src/components/home/Footer/Footer.js
@@ -31,24 +31,16 @@ const Footer = () => {
     <div className="w-full bg-[#F5F5F3] py-20">
       <div className="max-w-container mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-6 px-4 gap-10">
         <div className="col-span-2">
-          <FooterListTitle title=" More about Orebi Shop" />
+          <FooterListTitle title=" More about TrendyHub Shop" />
           <div className="flex flex-col gap-6">
             <p className="text-base w-full xl:w-[80%]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint
               ab ullam, numquam nesciunt in.
             </p>
             <ul className="flex items-center gap-2">
+            
               <a
-                href="https://www.youtube.com/@reactjsBD"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <li className="w-7 h-7 bg-primeColor text-gray-100 hover:text-white cursor-pointer text-lg rounded-full flex justify-center items-center hover:bg-black duration-300">
-                  <FaYoutube />
-                </li>
-              </a>
-              <a
-                href="https://github.com/noorjsdivs"
+                href="https://github.com/chandan24042001s"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -56,17 +48,9 @@ const Footer = () => {
                   <FaGithub />
                 </li>
               </a>
+            
               <a
-                href="https://www.facebook.com/Noorlalu143/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <li className="w-7 h-7 bg-primeColor text-gray-100 hover:text-white cursor-pointer text-lg rounded-full flex justify-center items-center hover:bg-black duration-300">
-                  <FaFacebook />
-                </li>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+                href="https://www.linkedin.com/in/chandan-kumal777/"
                 target="_blank"
                 rel="noreferrer"
               >
--- frontend/src/components/home/Footer/FooterBottom.js
+++ frontend/src/components/home/Footer/FooterBottom.js
@@ -5,17 +5,14 @@ const FooterBottom = () => {
   return (
     <div className="w-full bg-[#F5F5F3] group">
       <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
-        <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
+        <div className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
           <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
             <AiOutlineCopyright />
           </span>
-          Copyright 2022 | Orebi shopping | All Rights Reserved |
-          <a href="https://reactbd.com/" target="_blank" rel="noreferrer">
-            <span className="ml-1 font-medium group-hover:text-primeColor">
-              Powered by ReactBD.com
-            </span>
-          </a>
-        </p>
+          NoCopyRight -- Feel Free to Learn by cloning knowledge | #Developer
+          
+        
+        </div>
       </div>
     </div>
   );
